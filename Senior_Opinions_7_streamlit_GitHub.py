92274388f2af28cfbd825a12229c4973216c0deb
cplooo <cplo@kimo.com>
2024-01-29 16:08:52 +0800

    Add files via upload

--- Senior_Opinions_7_streamlit_GitHub.py
+++ Senior_Opinions_7_streamlit_GitHub.py
@@ -61,10 +61,12 @@ def adjust_df(df, order):
 
 
 df_streamlit = []
+column_title = []
 ####### Part1  
 ###### Part1-1 系師資素質與專長
 #df_senior.iloc[:,9] ## 1. 系師資素質與專長
 #df_senior.columns[9][3:]  ## '系師資素質與專長'
+column_title.append(df_senior.columns[9][3:])
 #type(df_senior.iloc[:,9])  ## pandas.core.series.Series
 
 ##### 计算不同子字符串的出现次数
@@ -91,6 +93,7 @@ df_streamlit.append(result_df_r)
 ###### Part1-2 系的教學品質
 #df_senior.iloc[:,10] ## 2. 系的教學品質
 #df_senior.columns[10][3:]  ## '系的教學品質'
+column_title.append(df_senior.columns[10][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,10].value_counts()
@@ -115,6 +118,7 @@ df_streamlit.append(result_df_r)
 ###### Part1-3 系上師生間的互動關係
 # df_senior.iloc[:,11] ## 3. 系上師生間的互動關係
 #df_senior.columns[11][3:]  ## '系上師生間的互動關係'
+column_title.append(df_senior.columns[11][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,11].value_counts()
@@ -139,6 +143,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part1-4 系課程內容
 # df_senior.iloc[:,12] ## 4. 系課程內容
+column_title.append(df_senior.columns[12][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,12].value_counts()
@@ -163,6 +168,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part1-5 系對學生思辨與探究能力的培養
 # df_senior.iloc[:,13] ## 5. 系對學生思辨與探究能力的培養
+column_title.append(df_senior.columns[13][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,13].value_counts()
@@ -187,6 +193,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part1-6 系對學生創新或創造力的培養
 # df_senior.iloc[:,14] ## 6. 系對學生創新或創造力的培養
+column_title.append(df_senior.columns[14][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,14].value_counts()
@@ -211,6 +218,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part1-7 系對學生在專業領域中具競爭力的培育
 # df_senior.iloc[:,15] ## 7. 系對學生在專業領域中具競爭力的培育
+column_title.append(df_senior.columns[15][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,15].value_counts()
@@ -236,6 +244,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part1-8 系修課規定
 # df_senior.iloc[:,16] ## 8.系修課規定
+column_title.append(df_senior.columns[16][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,16].value_counts()
@@ -261,6 +270,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part1-9 系的學習風氣
 # df_senior.iloc[:,17] ## 9. 系的學習風氣
+column_title.append(df_senior.columns[17][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,17].value_counts()
@@ -286,6 +296,7 @@ df_streamlit.append(result_df_r)
 ####### Part2  
 ###### Part2-1 系的空間環境與設備
 # df_senior.iloc[:,19] ## 1. 系的空間環境與設備
+column_title.append(df_senior.columns[19][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,19].value_counts()
@@ -310,6 +321,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part2-2 系行政人員的服務品質
 # df_senior.iloc[:,20] ## 2. 系行政人員的服務品質
+column_title.append(df_senior.columns[20][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,20].value_counts()
@@ -334,6 +346,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part2-3  系提供的工讀與獎助機會
 # df_senior.iloc[:,21] ## 3. 系提供的工讀與獎助機會
+column_title.append(df_senior.columns[21][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,21].value_counts()
@@ -358,6 +371,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part2-4 系提供的相關學習活動
 # df_senior.iloc[:,22] ## 4. 系提供的相關學習活動
+column_title.append(df_senior.columns[22][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,22].value_counts()
@@ -381,6 +395,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part2-5 系提供給學生的學習協助
 # df_senior.iloc[:,23] ## 5.系提供給學生的學習協助
+column_title.append(df_senior.columns[23][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,23].value_counts()
@@ -404,6 +419,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part2-6 系對學生的生涯輔導
 # df_senior.iloc[:,24] ## 6. 系對學生的生涯輔導
+column_title.append(df_senior.columns[24][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,24].value_counts()
@@ -427,6 +443,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part2-7 系對學生意見與需求的重視
 # df_senior.iloc[:,25] ## 7. 系對學生意見與需求的重視
+column_title.append(df_senior.columns[25][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,25].value_counts()
@@ -451,6 +468,7 @@ df_streamlit.append(result_df_r)
 ####### Part3  
 ###### Part3-1 目前就讀系的聲譽
 # df_senior.iloc[:,27] ## 1. 目前就讀系的聲譽
+column_title.append(df_senior.columns[27][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,27].value_counts()
@@ -475,6 +493,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part3-2 系的進步程度
 # df_senior.iloc[:,28] ## 2. 系的進步程度
+column_title.append(df_senior.columns[28][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,28].value_counts()
@@ -499,6 +518,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part3-3 系定位與特色
 # df_senior.iloc[:,29] ## 3. 系定位與特色
+column_title.append(df_senior.columns[29][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,29].value_counts()
@@ -540,6 +560,7 @@ df_streamlit.append(result_df_r)
 ####### Part4  
 ###### Part4-1 協助學生瞭解就業市場現況與產業發展趨勢
 # df_senior.iloc[:,33] ## 1. 協助學生瞭解就業市場現況與產業發展趨勢
+column_title.append(df_senior.columns[33][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,33].value_counts()
@@ -564,6 +585,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part4-2 協助學生生涯發展與規劃
 # df_senior.iloc[:,34] ## 2. 協助學生生涯發展與規劃
+column_title.append(df_senior.columns[34][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,34].value_counts()
@@ -589,6 +611,8 @@ df_streamlit.append(result_df_r)
 
 ###### Part4-3 生涯與就業輔導服務品質
 # df_senior.iloc[:,35] ## 3. 生涯與就業輔導服務品質
+# df_senior.columns[35]
+column_title.append(df_senior.columns[35][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,35].value_counts()
@@ -616,6 +640,7 @@ df_streamlit.append(result_df_r)
 ####### Part5  課程規劃與教師教學滿意度(依多數課程情況回答)
 ###### Part5-1 提供國外修課、實習或交換學生機會
 # df_senior.iloc[:,37] ## 1. 提供國外修課、實習或交換學生機會
+column_title.append(df_senior.columns[37][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,37].value_counts()
@@ -639,6 +664,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part5-2 提供與外籍人士或國際社群互動交流的機會
 # df_senior.iloc[:,38] ## 2. 提供與外籍人士或國際社群互動交流的機會
+column_title.append(df_senior.columns[38][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,38].value_counts()
@@ -663,6 +689,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part5-3 外語學習機會與環境
 # df_senior.iloc[:,39] ## 3.外語學習機會與環境
+column_title.append(df_senior.columns[39][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,39].value_counts()
@@ -687,9 +714,11 @@ df_streamlit.append(result_df_r)
 
 ###### Part5-4 提供瞭解外國政治、經濟、社會、文化情況的機會
 # df_senior.iloc[:,40] ## 4. 提供瞭解外國政治、經濟、社會、文化情況的機會
+column_title.append(df_senior.columns[40][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,40].value_counts()
+column_title.append(df_senior.columns[40][3:])
 
 ##### 计算不同子字符串的比例
 proportions = value_counts / value_counts.sum()
@@ -712,6 +741,7 @@ df_streamlit.append(result_df_r)
 ####### Part6  
 ###### Part6-1 校園環境規劃與維護
 # df_senior.iloc[:,42] ## 1. 校園環境規劃與維護
+column_title.append(df_senior.columns[42][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,42].value_counts()
@@ -735,6 +765,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part6-2 校園內的安全保障
 # df_senior.iloc[:,43] ## 2. 校園內的安全保障
+column_title.append(df_senior.columns[43][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,43].value_counts()
@@ -758,6 +789,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part6-3 您是否申請或參與過「職涯輔導」 學習輔導方案或輔導活動嗎?
 # df_senior.iloc[:,44] ## 3. 學校各項收費
+column_title.append(df_senior.columns[44][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,44].value_counts()
@@ -781,6 +813,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part6-4 您是否申請或參與過「外語教學中心學習輔導」 學習輔導方案或輔導活動嗎?
 # df_senior.iloc[:,45] ## 4. 電腦網路設備
+column_title.append(df_senior.columns[45][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,45].value_counts()
@@ -804,6 +837,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part6-5 您是否申請或參與過「諮商暨健康中心的諮商輔導」 學習輔導方案或輔導活動嗎?
 # df_senior.iloc[:,46] ## 5. 運動休閒設施
+column_title.append(df_senior.columns[46][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,46].value_counts()
@@ -827,6 +861,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part6-6 學校生活機能便利性
 # df_senior.iloc[:,47] ## 6. 學校生活機能便利性
+column_title.append(df_senior.columns[47][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,47].value_counts()
@@ -849,6 +884,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part6-7 學生宿舍數量
 # df_senior.iloc[:,48] ## 7. 學生宿舍數量
+column_title.append(df_senior.columns[48][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,48].value_counts()
@@ -873,6 +909,7 @@ df_streamlit.append(result_df_r)
 ####### Part7  
 ###### Part7-1 學校的聲譽
 # df_senior.iloc[:,50] ## 1. 學校的聲譽
+column_title.append(df_senior.columns[50][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,50].value_counts()
@@ -896,6 +933,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part7-2 學校的進步程度
 # df_senior.iloc[:,51] ## 2. 學校的進步程度
+column_title.append(df_senior.columns[51][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,51].value_counts()
@@ -919,6 +957,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part7-3 學校定位與特色
 # df_senior.iloc[:,52] ## 3. 學校定位與特色
+column_title.append(df_senior.columns[52][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,52].value_counts()
@@ -942,6 +981,7 @@ df_streamlit.append(result_df_r)
 
 ###### Part7-4 學校學風自由開放程度
 # df_senior.iloc[:,53] ## 4. 學校學風自由開放程度
+column_title.append(df_senior.columns[53][3:])
 
 ##### 计算不同子字符串的出现次数
 value_counts = df_senior.iloc[:,53].value_counts()
@@ -968,23 +1008,6 @@ df_streamlit.append(result_df_r)
 ###### Part8-1 如果可以重來，您是否仍會就讀同一主修領域、學群或學類？
 # df_senior.iloc[:,55] ## 1. 如果可以重來，您是否仍會就讀同一主修領域、學群或學類？
 
-##### 计算不同子字符串的出现次数
-value_counts = df_senior.iloc[:,55].value_counts()
-
-##### 计算不同子字符串的比例
-proportions = value_counts / value_counts.sum()
-
-##### 轉換成 numpy array
-value_counts_numpy = value_counts.values
-proportions_numpy = proportions.values
-satisfaction_numpy = proportions.index.to_numpy()
-
-##### 创建一个新的DataFrame来显示结果
-result_df = pd.DataFrame({'滿意度':satisfaction_numpy, '人數': value_counts_numpy,'比例': proportions_numpy.round(4)})
-#### 調整滿意度次序
-result_df_r = adjust_df(result_df, desired_order)
-#### 存到 list 'df_streamlit'
-df_streamlit.append(result_df_r)
 
 
 
@@ -993,46 +1016,11 @@ df_streamlit.append(result_df_r)
 ###### Part8-2 如果可以重來，您是否仍會就讀本校的同一系？
 # df_senior.iloc[:,56] ## 2. 如果可以重來，您是否仍會就讀本校的同一系？
 
-##### 计算不同子字符串的出现次数
-value_counts = df_senior.iloc[:,56].value_counts()
-
-##### 计算不同子字符串的比例
-proportions = value_counts / value_counts.sum()
-
-##### 轉換成 numpy array
-value_counts_numpy = value_counts.values
-proportions_numpy = proportions.values
-satisfaction_numpy = proportions.index.to_numpy()
-
-##### 创建一个新的DataFrame来显示结果
-result_df = pd.DataFrame({'滿意度':satisfaction_numpy, '人數': value_counts_numpy,'比例': proportions_numpy.round(4)})
-#### 調整滿意度次序
-result_df_r = adjust_df(result_df, desired_order)
-#### 存到 list 'df_streamlit'
-df_streamlit.append(result_df_r)
-
 
 
 ###### Part8-3 如果可以重來，您是否仍會就讀本校？
 # df_senior.iloc[:,57] ## 3. 如果可以重來，您是否仍會就讀本校？
 
-##### 计算不同子字符串的出现次数
-value_counts = df_senior.iloc[:,57].value_counts()
-
-##### 计算不同子字符串的比例
-proportions = value_counts / value_counts.sum()
-
-##### 轉換成 numpy array
-value_counts_numpy = value_counts.values
-proportions_numpy = proportions.values
-satisfaction_numpy = proportions.index.to_numpy()
-
-##### 创建一个新的DataFrame来显示结果
-result_df = pd.DataFrame({'滿意度':satisfaction_numpy, '人數': value_counts_numpy,'比例': proportions_numpy.round(4)})
-#### 調整滿意度次序
-result_df_r = adjust_df(result_df, desired_order)
-#### 存到 list 'df_streamlit'
-df_streamlit.append(result_df_r)
 
 
 
@@ -1140,8 +1128,8 @@ for i in range(0, 28, 3):   ## 1, 4, 7, 10,13,16,19,22,25,28
     with col1:
         #st.write("<b>系師資素質與專長:</b>", result_df_rr.to_html(index=False), unsafe_allow_html=True)  ##不显示索引
         ## 创建带有HTML标签的字符串
-        column_title = df_senior.columns[i+0+9][3:]
-        html_content = f"<div style='text-align: center;'><b style='font-size: 15px;'>{column_title}</b></div>{df_streamlit[i+0].to_html(index=False)}"
+        # column_title = df_senior.columns[i+0+9][3:]
+        html_content = f"<div style='text-align: center;'><b style='font-size: 15px;'>{column_title[i+0]}</b></div>{df_streamlit[i+0].to_html(index=False)}"
         ## 自定义样式，包括表格宽度、字体大小和列名居中
         html_content = html_content.replace('<table border="1" class="dataframe">', 
                                             '<table style="width:105%; font-size: 12px; margin-left: auto; margin-right: auto;" align="center">')
@@ -1151,8 +1139,8 @@ for i in range(0, 28, 3):   ## 1, 4, 7, 10,13,16,19,22,25,28
     with col2:
         #st.write("<b>系的教學品質:</b>", result_df_rr.to_html(index=False), unsafe_allow_html=True)  ## 不显示索引
         ## 创建带有HTML标签的字符串
-        column_title = df_senior.columns[i+1+9][3:]
-        html_content = f"<div style='text-align: center;'><b style='font-size: 15px;'>{column_title}</b></div>{df_streamlit[i+1].to_html(index=False)}"
+        # column_title = df_senior.columns[i+1+9][3:]
+        html_content = f"<div style='text-align: center;'><b style='font-size: 15px;'>{column_title[i+1]}</b></div>{df_streamlit[i+1].to_html(index=False)}"
         ## 自定义样式，包括表格宽度、字体大小和列名居中
         html_content = html_content.replace('<table border="1" class="dataframe">', 
                                             '<table style="width:105%; font-size: 12px; margin-left: auto; margin-right: auto;" align="center">')
@@ -1162,8 +1150,8 @@ for i in range(0, 28, 3):   ## 1, 4, 7, 10,13,16,19,22,25,28
     with col3:
         #st.write("<b>系上師生間的互動關係:</b>", result_df_rr.to_html(index=False), unsafe_allow_html=True)  ## 不显示索引
         ## 创建带有HTML标签的字符串
-        column_title = df_senior.columns[i+2+9][3:]
-        html_content = f"<div style='text-align: center;'><b style='font-size: 15px;'>{column_title}</b></div>{df_streamlit[i+2].to_html(index=False)}"
+        # column_title = df_senior.columns[i+2+9][3:]
+        html_content = f"<div style='text-align: center;'><b style='font-size: 15px;'>{column_title[i+2]}</b></div>{df_streamlit[i+2].to_html(index=False)}"
         ## 自定义样式，包括表格宽度、字体大小和列名居中
         html_content = html_content.replace('<table border="1" class="dataframe">', 
                                             '<table style="width:105%; font-size: 12px; margin-left: auto; margin-right: auto;" align="center">')
